5244fe3d6a9d9688807e14fc3fbcd1a103fbb530
Zexal0807 <gallinar00@gmail.com>
2020-12-28 21:22:31 +0100

    Add check argument number in ReceiverManager

--- receiver_manager.c
+++ receiver_manager.c
@@ -9,6 +9,13 @@
 #include "pipe.h"
 
 int main(int argc, char * argv[]) {
+
+    // Check command line input arguments
+    if (argc != 0){
+        printf("Error invocation of Receiver Manager");
+        return 1;
+    }
+
     // Define the 3 struct process
     process *R1 = null;
     process *R2 = null;
