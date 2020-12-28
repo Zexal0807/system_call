6911c290c1260d8c8e43b1d40d10146844296513
Zexal0807 <gallinar00@gmail.com>
2020-12-28 21:22:53 +0100

    Add creation of the child in ReceiverManager

--- receiver_manager.c
+++ receiver_manager.c
@@ -20,8 +20,45 @@ int main(int argc, char * argv[]) {
     process *R1 = null;
     process *R2 = null;
     process *R3 = null;
+
+    // Try to create a child, i
+    int pid = fork();
+    if(pid == -1){  
+        printf("Error invocation of Receiver Manager");
+        return 1;
+    }else if(pid == 0){
+        R1();
+        exit(0);
+    }
     R1 = createProcess('R', 1, pid);
+    
+    pid = fork();
+    if(pid == -1){  return 0;
+    }else if(pid == 0){
+        R2();
+        exit(0);
+    }
     R2 = createProcess('R', 2, pid);
+
+    pid = fork();
+    if(pid == -1){  return 0;
+    }else if(pid == 0){
+        R3();
+        exit(0);
+    }
     R3 = createProcess('R', 3, pid);
+
     return 0;
+}
+
+void R1(){
+    printf("Call Reciver 1");
+}
+
+void R2(){
+    printf("Call Reciver 2");
+}
+
+void R3(){
+    printf("Call Reciver 3");
 }
