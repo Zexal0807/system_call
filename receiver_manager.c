939b913ed7076fc7ff7319a53959dd7ca2ac59b2
Zexal0807 <gallinar00@gmail.com>
2020-12-28 21:22:08 +0100

    Add createProcess call in receiverManager

--- receiver_manager.c
+++ receiver_manager.c
@@ -9,5 +9,12 @@
 #include "pipe.h"
 
 int main(int argc, char * argv[]) {
+    // Define the 3 struct process
+    process *R1 = null;
+    process *R2 = null;
+    process *R3 = null;
+    R1 = createProcess('R', 1, pid);
+    R2 = createProcess('R', 2, pid);
+    R3 = createProcess('R', 3, pid);
     return 0;
 }
