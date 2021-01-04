586f3554fb62d1b83afd36b776cd3f367000241e
Zexal0807 <gallinar00@gmail.com>
2021-01-04 10:53:37 +0100

    Fix - identation

--- hackler.c
+++ hackler.c
@@ -12,16 +12,16 @@
 #include <unistd.h>
 
 int main(int argc, char * argv[]) {
-  if (argc != 2){
-    printf("Error invocation of Hackler, you must pass the input file");
-    return 1;
-  }
-  // Start process
-  printLog("HK", "Process start");
+	if (argc != 2){
+		printf("Error invocation of Hackler, you must pass the input file");
+		return 1;
+	}
+	// Start process
+	printLog("HK", "Process start");
 
 
-  // Wait for 2 second befor end
-  sleep(2);
-  printLog("HK", "Process end");
-  return 0;
+	// Wait for 2 second befor end
+	sleep(2);
+	printLog("HK", "Process end");
+	return 0;
 }
