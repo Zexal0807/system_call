bfefd8f2f3ff8b9d8ff3e805b97aca41a787d01d
repl.it user <replituser@example.com>
2021-01-04 19:22:19 +0000

    Implement printTrafficInfo

--- process/R1.c
+++ process/R1.c
@@ -14,7 +14,28 @@
 int main(int argc, char * argv[]) {
     printLog("R1", "Process start with exec");
     
-    printf("%s %s", argv[0],argv[1]);
+    time_t a;
+    time_t d;
+    time ( &a );
+    time ( &d );
+
+    trafficInfo *t = createTrafficInfo(
+      createMessage(
+        1, 
+        "Ciao come va?",
+        createProcess('S', 1, 1),
+        createProcess('R', 1, 1),
+        1,
+        1,
+        1,
+        "H"
+      ),
+      a,
+      d
+    );
+
+    printTrafficInfo("output/F1.csv",t);
+
     // Wait for 1 second befor end
     sleep(1);
     printLog("R1", "Process End");
