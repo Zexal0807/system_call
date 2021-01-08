5d067e6cdcad9e613231d4d73667f6deeab72cb4
Zexal0807 <gallinar00@gmail.com>
2021-01-08 08:39:07 +0100

    Add filename const

--- defines.h
+++ defines.h
@@ -11,6 +11,18 @@
 #include "struct/hacklerAction.h"
 #include "struct/list.h"
 
+#ifndef FILENAME
+#define FILENAME
+#define SENDER_FILENAME = "output/F8.csv"
+#define SENDER_1_FILENAME = "output/F1.csv"
+#define SENDER_2_FILENAME = "output/F2.csv"
+#define SENDER_3_FILENAME = "output/F3.csv"
+#define RECEIVER_FILENAME = "output/F9.csv"
+#define RECEIVER_1_FILENAME = "output/F4.csv"
+#define RECEIVER_2_FILENAME = "output/F5.csv"
+#define RECEIVER_3_FILENAME = "output/F6.csv"
+#endif
+
 /*
 	Procedura per stampare a console un log di quello che avviene
 */
