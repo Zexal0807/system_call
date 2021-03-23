0fd1d90bdfc3f6bf7b419b37e36e18af4428e8af
repl.it user <replituser@example.com>
2021-03-23 14:19:22 +0000

    Add sem lib

--- semaphore.c
+++ semaphore.c
@@ -1,5 +1,41 @@
 /// @file semaphore.c
 /// @brief Contiene l'implementazione delle funzioni specifiche per la gestione dei SEMAFORI.
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
 
 #include "err_exit.h"
 #include "semaphore.h"
+#include "defines.h"
+
+int generateSemaphore(key_t key, int n, int flags){
+    int id = semget(key, n, flags);
+    if (id == -1){
+        ErrExit("semget failed");
+    }
+    return id;
+}
+
+int createInitSemaphore(){
+    key_t key = generateKey(KEY_INIT_SEM);
+    int id = generateSemaphore(key, 4, S_IRUSR | S_IWUSR | IPC_CREAT);
+    return id;
+}
+
+int getInitSemaphore(){
+    key_t key = generateKey(KEY_INIT_SEM);
+    int id = generateSemaphore(key, 4, S_IRUSR | S_IWUSR );
+    return id;
+}
+
+void semOp (int semid, unsigned short sem_num, short sem_op) {
+    struct sembuf sop = { 
+      sem_num,
+      sem_op,
+      0
+    };
+
+    if (semop(semid, &sop, 1) == -1)
+        ErrExit("semop failed");
+}
+
--- semaphore.h
+++ semaphore.h
@@ -2,3 +2,16 @@
 /// @brief Contiene la definizioni di variabili e funzioni specifiche per la gestione dei SEMAFORI.
 
 #pragma once
+/*
+struct sembuf {
+    unsigned short sem_num;
+    short sem_op;
+    short sem_flg;
+};
+*/
+int generateSemaphore(key_t key, int n, int flags);
+
+int createInitSemaphore();
+int getInitSemaphore();
+
+void semOp (int semid, unsigned short sem_num, short sem_op);
